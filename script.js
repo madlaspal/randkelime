0a04bc476d48198e2260df607eebb70ac5f3b404
madlaspal <madlaspal7@gmail.com>
2026-07-04 05:58:28 +0100

    google giriş eklendi

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ button.addEventListener("click", function () {
     turkish.textContent = words[randomIndex].mean;
 
 });
+let currentLang = "tr";
+
+const translations = {
+    tr: {
+        title: "Kelime Öğren",
+        button: "Yeni Kelime"
+    },
+    en: {
+        title: "Learn Words",
+        button: "New Word"
+    },
+    de: {
+        title: "Wörter lernen",
+        button: "Neues Wort"
+    },
+    fr: {
+        title: "Apprendre les mots",
+        button: "Nouveau mot"
+    },
+    ar: {
+        title: "تعلم الكلمات",
+        button: "كلمة جديدة"
+    }
+};
+
+function setLang(lang){
+    currentLang = lang;
+
+    document.querySelector(".title").textContent = translations[lang].title;
+    document.querySelector(".btn").textContent = translations[lang].button;
+
+    // burası ileride JSON dil değişimi için
+    console.log("Dil değişti:", lang);
+}
